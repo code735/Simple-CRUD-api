34a2ed4be11d713f9e67f7783f0331cd0cb7c10d
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2022-12-05 22:53:02 +0530

    index users

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,22 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName:"Prathmesh",
+        lastName : "Kumbhar",
+        age : 21
+    },
+    {
+        firstName:"Sushant",
+        lastName : "Kumbhar",
+        age : 18
+    }
+]
 
 //all routes in here wstarting with /users
 router.get('/',(req,res) => {
+    console.log(users);
     res.send('Hello');
 });
 
